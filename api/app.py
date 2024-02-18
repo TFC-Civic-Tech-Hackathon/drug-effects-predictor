f292b139ebc0c52abc0bbbe18c02418e9ab062fa
nairabhi96 <nair.abhi@northeastern.edu>
2024-02-18 02:38:46 -0500

    added drug initial load to the database

--- api/app.py
+++ api/app.py
@@ -28,22 +28,30 @@ def signup():
 # 
 @app.route('/upload', methods=['POST'])
 def upload_csv():
+
+    # Remove tables before inserting
+    
     # Check if files are provided in the request
-    if 'medicine_dataset' not in request.files or 'patient_records' not in request.files:
+    if 'medicine_dataset' not in request.files or 'patient_records' not in request.files or 'drug_effects' not in request.files:
         return jsonify({'error': 'CSV files not provided'})
 
-    # Get drug file and patient file from the request
+    # Get drug file, patient file, and drug effects file from the request
     drug_file = request.files['medicine_dataset']
     patient_file = request.files['patient_records']
+    drug_effects_file = request.files['drug_effects']
 
     # Read only the first 50 drug rows
     drug_data = list(csv.DictReader(io.StringIO(drug_file.read().decode('utf-8'))))[:50]
 
-    # Reset file pointer for patient file
+    # Reset file pointers for patient and drug effects files
     patient_file.seek(0)
+    drug_effects_file.seek(0)
 
     # Read patient records CSV file
     patient_data = list(csv.DictReader(io.StringIO(patient_file.read().decode('utf-8'))))
+
+    # Read drug effects CSV file
+    drug_effects_data = list(csv.DictReader(io.StringIO(drug_effects_file.read().decode('utf-8'))))
 
     # Number of patients
     num_patients = len(patient_data)
@@ -52,6 +60,16 @@ def upload_csv():
     patients_collection = db['Patients_record']
     for patient_row in patient_data:
         patients_collection.insert_one(patient_row)
+
+    # Create "Drug effects" table and insert drug effects data
+    drug_effects_collection = db['Drug_effects']
+    for drug_effect_row in drug_effects_data:
+        drug_effects_collection.insert_one(drug_effect_row)
+
+
+    drug_file_collection = db['drug_file']
+    for drug_file_row in drug_data:
+        drug_file_collection.insert_one(drug_file_row)
 
     # Assign drugs to patients in a circular manner
     drug_index = 0
@@ -75,7 +93,6 @@ def upload_csv():
 
         # Move to the next drug index in a circular manner
         drug_index = (drug_index + 1) % len(drug_data)
-
 
     return jsonify({'success': 'Data uploaded successfully'})
 
